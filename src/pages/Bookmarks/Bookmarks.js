427bf629d14923f9a1e105226df38c162200a827
HussainAhmed1117 <31531817+HussainAhmed1117@users.noreply.github.com>
2023-11-02 14:48:57 +0530

    first change

---
+++ src/pages/Bookmarks/Bookmarks.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Page.css'
+export const Bookmarks = () => {
+  return (
+    <div className='page'>
+        <h2 className='pageTitle'>Welcome to Bookmarks</h2>
+    </div>
+  )
+}
+export default Bookmarks;
